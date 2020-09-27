807aa7eea784cc91c5167c4eb625288b3198ddfb
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2020-09-27 16:50:06 +0300

    14 blog todo

---
+++ 14_mysql_blog/index.php
@@ -0,0 +1,23 @@
+<?
+/*
+
+	--- Структура папок: ---
+		index.php - точка входа
+		assets	- html, js, css (ресурсы)
+		core	- ядро системы (все необходимое для работы приложения)
+
+
+	--- url (ЧПУ) ---
+		будем уходить от    mysite/index.php?catalog=asd
+		к url вида         mysite/catalog/asd
+		для этого создаем .htaccess (правила для веб сервера)
+
+
+36 минут
+
+
+ */
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
